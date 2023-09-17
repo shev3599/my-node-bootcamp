a8566494ec98f529a4a8bd94e0c049afe225c177
Maxim Shevchenko <maxshev2000@gmail.com>
2023-09-17 10:56:24 +0200

    lenovo index

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 //console.log('fs.readFileSync:', fs.readFileSync('./txt/input.txt'));
 // const hello = 'Hello World!'
 // console.log('hello:', hello);
@@ -15,18 +14,26 @@ const fs = require('fs');
 
 // Non-blocking asynchronous way
 
-fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
-    console.log('data1',data1);
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
-        console.log('data1-1',data1);
-        console.log('data2',data2);
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            console.log('data3:', data3);
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}\n${data2}`,'utf-8',err =>{
-                console.log('Your file has been written!;)');
-            })
-        })
-    })
-})
+// fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
+//     console.log('data1',data1);
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log('data1-1',data1);
+//         console.log('data2',data2);
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//             console.log('data3:', data3);
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}\n${data2}`,'utf-8',err =>{
+//                 console.log('Your file has been written!;)');
+//             })
+//         })
+//     })
+// })
 
-console.log('Will read file:');
+// console.log('Will read file:');
+
+//Server
+
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const 
