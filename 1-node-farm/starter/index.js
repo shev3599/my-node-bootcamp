86e91a948c57626c57659b6daf9c11ba8f7203f0
Maxim Shevchenko <maxshev2000@gmail.com>
2023-09-18 18:32:25 +0200

    routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -36,4 +36,24 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-const 
+const server = http.createServer((req,res)=>{
+    //console.log(req.url)
+    pathName = req.url
+    
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is an OVERVEW page!')
+    }else if(pathName === '/product'){
+        res.end('This is a PRODUCT page!')
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html',
+            'my-own-header':'hello-world'
+        })
+        res.end('<h1>This page cannot be found!</h1>')
+    }
+
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listeiing to requests on port 8000:');
+});
