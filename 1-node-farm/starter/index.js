73c4f211b76d4ce32cef6bf27d2626b060e15411
Maxim Shevchenko <maxshev2000@gmail.com>
2023-09-25 04:54:59 +0200

    fix

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -36,6 +36,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/`,'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
