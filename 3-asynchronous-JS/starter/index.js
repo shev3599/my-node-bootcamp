2596f0fc172b4421dd7fc7069907195c240787bf
Maxim Shevchenko <maxshev2000@gmail.com>
2023-11-19 12:41:18 +0100

    update nov1923

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,18 +19,18 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((data) => {
-    console.log('data.body.message:', data.body.message);
-    return writeFilePro('dog-image.txt', data.body.message);
-  })
-  .then(() => {
-    console.log('random dog image saved to the file');
-  })
-  .catch((err) => {
-    console.log('Catch err:', err.message);
-  });
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((data) => {
+//     console.log('data.body.message:', data.body.message);
+//     return writeFilePro('dog-image.txt', data.body.message);
+//   })
+//   .then(() => {
+//     console.log('random dog image saved to the file');
+//   })
+//   .catch((err) => {
+//     console.log('Catch err:', err.message);
+//   });
