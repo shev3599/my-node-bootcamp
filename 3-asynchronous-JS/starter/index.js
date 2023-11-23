d420a2984a3042594726caa5098d93d675d8dd4f
Maxim Shevchenko <maxshev2000@gmail.com>
2023-11-23 10:04:19 +0100

    upade nov2311

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const superagent = require('superagent');
+const { isArray } = require('util');
 
-const readFilePro = (file) => {
+const readFilePro = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
-      if (err) reject('Cannot read file! Sorry!!!');
+      if (err) reject('Cannot read file! Sorry!!! 😢');
       resolve(data);
     });
   });
@@ -12,13 +13,58 @@ const readFilePro = (file) => {
 
 const writeFilePro = (file, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, (err) => {
-      if (err) reject('Could not write file 😢!');
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write file 😢!⛽');
       resolve('File was written with success');
     });
   });
 };
 
+const DogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}\\dog.txt`);
+    console.log(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const data1 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const data2 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const data3 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const all = await Promise.all([data1, data2, data3]);
+    const imgs = all.map(el => el.body.message).join('\n');
+    console.log('isArray all', Array.isArray(all));
+    console.log('imgs:', imgs);
+
+    await writeFilePro('dog-image.txt', imgs);
+    console.log('Random dog image saved to the file');
+  } catch (err) {
+    console.log('Catch err:', err.message);
+    throw err;
+  }
+  return '2: READY 🐕';
+};
+
+(async () => {
+  try {
+    console.log('1: Will getting log pics!');
+    const x = await DogPic();
+    console.log('x', x);
+    console.log('3: Done getting log pics!');
+  } catch (err) {
+    console.log('ERROR 💣', err);
+  }
+})();
+
+/*
+console.log('1: Will getting log pics!');
+DogPic()
+  .then(x => {
+    console.log('2x: ', x);
+    console.log('3: Done getting log pics!');
+  })
+  .catch(err => {
+    console.log('ERROR 💣', err);
+  });
+*/
 // readFilePro(`${__dirname}/dog.txt`)
 //   .then((data) => {
 //     console.log(`Breed: ${data}`);
---
+++ 3-asynchronous-JS/starter/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const x = 34;
+birthYear = 1963;
+
+const calcAge = birthYear => 2023 - birthYear;
+
+console.log('Age = ', calcAge(birthYear));
